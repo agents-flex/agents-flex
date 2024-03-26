4a5d4abf5236a2d55fff5e5f66ca34a9de656efd
Michael Yang <fuhai999@gmail.com>
2024-03-26 13:01:16 +0800

    chore: update ChatglmLlm Copyright

--- agents-flex-llm/agents-flex-llm-chatglm/src/main/java/com/agentsflex/llm/chatglm/ChatglmLlm.java
+++ agents-flex-llm/agents-flex-llm-chatglm/src/main/java/com/agentsflex/llm/chatglm/ChatglmLlm.java
@@ -1,7 +1,19 @@
+/*
+ *  Copyright (c) 2022-2023, Agents-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.agentsflex.llm.chatglm;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import com.agentsflex.document.Document;
 import com.agentsflex.llm.BaseLlm;
@@ -25,6 +37,9 @@ import com.agentsflex.store.VectorData;
 import com.agentsflex.util.Maps;
 import com.agentsflex.util.StringUtil;
 import com.alibaba.fastjson.JSONPath;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatglmLlm extends BaseLlm<ChatglmLlmConfig> {
 
