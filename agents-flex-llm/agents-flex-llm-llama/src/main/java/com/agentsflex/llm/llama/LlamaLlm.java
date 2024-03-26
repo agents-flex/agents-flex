64e5b6cdd8a16e5752e9911e30afc8913e4c8e16
Michael Yang <fuhai999@gmail.com>
2024-03-26 12:59:46 +0800

    feat: add LlaMa LLM support

---
+++ agents-flex-llm/agents-flex-llm-llama/src/main/java/com/agentsflex/llm/llama/LlamaLlm.java
@@ -0,0 +1,94 @@
+/*
+ *  Copyright (c) 2022-2023, Agents-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.agentsflex.llm.llama;
+
+import com.agentsflex.document.Document;
+import com.agentsflex.llm.BaseLlm;
+import com.agentsflex.llm.MessageListener;
+import com.agentsflex.llm.MessageResponse;
+import com.agentsflex.llm.client.BaseLlmClientListener;
+import com.agentsflex.llm.client.HttpClient;
+import com.agentsflex.llm.client.LlmClient;
+import com.agentsflex.llm.client.LlmClientListener;
+import com.agentsflex.llm.client.impl.SseClient;
+import com.agentsflex.llm.response.AiMessageResponse;
+import com.agentsflex.message.AiMessage;
+import com.agentsflex.message.Message;
+import com.agentsflex.parser.AiMessageParser;
+import com.agentsflex.prompt.FunctionPrompt;
+import com.agentsflex.prompt.Prompt;
+import com.agentsflex.store.VectorData;
+import com.agentsflex.util.StringUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LlamaLlm extends BaseLlm<LlamaLlmConfig> {
+
+    private HttpClient httpClient = new HttpClient();
+    public AiMessageParser aiMessageParser = LlamaLlmUtil.getAiMessageParser();
+//    public FunctionMessageParser functionMessageParser = LlamaLlmUtil.getFunctionMessageParser();
+
+
+    public LlamaLlm(LlamaLlmConfig config) {
+        super(config);
+    }
+
+
+    @Override
+    public VectorData embeddings(Document document) {
+        return null;
+    }
+
+
+    @Override
+    public <R extends MessageResponse<M>, M extends Message> R chat(Prompt<M> prompt) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Authorization", "Bearer " + config.getApiKey());
+
+        String endpoint = config.getEndpoint();
+        String payload = LlamaLlmUtil.promptToPayload(prompt, config, false);
+        String responseString = httpClient.post(endpoint + "/v1/chat/completions", headers, payload);
+        if (StringUtil.noText(responseString)) {
+            return null;
+        }
+
+        if (prompt instanceof FunctionPrompt) {
+            throw new IllegalStateException("Llama LLM can not support function prompt.");
+        } else {
+            AiMessage aiMessage = aiMessageParser.parse(responseString);
+            return (R) new AiMessageResponse(aiMessage);
+        }
+    }
+
+
+    @Override
+    public <R extends MessageResponse<M>, M extends Message> void chatAsync(Prompt<M> prompt, MessageListener<R, M> listener) {
+        LlmClient llmClient = new SseClient();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Authorization", "Bearer " + config.getApiKey());
+
+        String payload = LlamaLlmUtil.promptToPayload(prompt, config, true);
+
+        String endpoint = config.getEndpoint();
+        LlmClientListener clientListener = new BaseLlmClientListener(this, llmClient, listener, prompt, aiMessageParser, null);
+        llmClient.start(endpoint + "/api/paas/v4/chat/completions", headers, payload, clientListener, config);
+    }
+
+
+}
---
+++ agents-flex-llm/agents-flex-llm-llama/src/main/java/com/agentsflex/llm/llama/LlamaLlmUtil.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (c) 2022-2023, Agents-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.agentsflex.llm.llama;
+
+import com.agentsflex.message.AiMessage;
+import com.agentsflex.message.MessageStatus;
+import com.agentsflex.parser.AiMessageParser;
+import com.agentsflex.parser.impl.BaseAiMessageParser;
+import com.agentsflex.prompt.DefaultPromptFormat;
+import com.agentsflex.prompt.Prompt;
+import com.agentsflex.prompt.PromptFormat;
+import com.agentsflex.util.Maps;
+
+public class LlamaLlmUtil {
+
+    private static final PromptFormat promptFormat = new DefaultPromptFormat();
+
+
+    public static AiMessageParser getAiMessageParser() {
+        BaseAiMessageParser aiMessageParser = new BaseAiMessageParser() {
+            @Override
+            public AiMessage parse(String content) {
+                if ("[DONE]".equals(content)) {
+                    return null;
+                }
+                return super.parse(content);
+            }
+        };
+        aiMessageParser.setContentPath("$.choices[0].message.content");
+        aiMessageParser.setIndexPath("$.choices[0].index");
+        aiMessageParser.setStatusPath("$.choices[0].finish_reason");
+        aiMessageParser.setStatusParser(content -> parseMessageStatus((String) content));
+        aiMessageParser.setTotalTokensPath("$.usage.total_tokens");
+        return aiMessageParser;
+    }
+
+
+
+    public static String promptToPayload(Prompt prompt, LlamaLlmConfig config, boolean stream) {
+        return Maps.of("model", config.getModel())
+            .put("messages", promptFormat.toMessagesJsonKey(prompt))
+            .toJSON();
+
+    }
+
+
+    public static MessageStatus parseMessageStatus(String status) {
+        return "stop".equals(status) ? MessageStatus.END : MessageStatus.MIDDLE;
+    }
+
+
+}
