9600c49fff304d7cd4a960c855c0af0fb21a3ec4
Michael Yang <fuhai999@gmail.com>
2024-05-08 19:26:25 +0800

    test: update test

--- agents-flex-core/src/test/java/com/agentsflex/core/test/io/IOChainTest.java
+++ agents-flex-core/src/test/java/com/agentsflex/core/test/io/IOChainTest.java
@@ -1,5 +1,8 @@
 package com.agentsflex.core.test.io;
 
+import com.agentsflex.chain.Chain;
+import com.agentsflex.chain.ChainEvent;
+import com.agentsflex.chain.ChainEventListener;
 import com.agentsflex.chain.IOChain;
 
 public class IOChainTest {
@@ -14,6 +17,13 @@ public class IOChainTest {
         ioChain2.addNode(new IOAgent1("agent3"));
         ioChain2.addNode(new IOAgent2("agent4"));
         ioChain2.addNode(ioChain1);
+
+        ioChain2.registerEventListener(new ChainEventListener() {
+            @Override
+            public void onEvent(ChainEvent event, Chain chain) {
+                System.out.println(event);
+            }
+        });
 
 
         Object result = ioChain2.execute("your params");
