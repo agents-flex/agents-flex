90ce491acb42c195973af2501401f56e8e102c88
Michael Yang <fuhai999@gmail.com>
2024-05-20 16:57:20 +0800

    refactor: optimize HttpClient.java

--- agents-flex-core/src/main/java/com/agentsflex/llm/client/HttpClient.java
+++ agents-flex-core/src/main/java/com/agentsflex/llm/client/HttpClient.java
@@ -35,32 +35,31 @@ public class HttpClient {
             .build();
     }
 
-
     public HttpClient(OkHttpClient okHttpClient) {
         this.okHttpClient = okHttpClient;
     }
 
     public String get(String url) {
-        return method(url, "GET", null, null);
+        return execute(url, "GET", null, null);
     }
 
     public String get(String url, Map<String, String> headers) {
-        return method(url, "GET", headers, null);
+        return execute(url, "GET", headers, null);
     }
 
     public String post(String url, Map<String, String> headers, String payload) {
-        return method(url, "POST", headers, payload);
+        return execute(url, "POST", headers, payload);
     }
 
     public String put(String url, Map<String, String> headers, String payload) {
-        return method(url, "PUT", headers, payload);
+        return execute(url, "PUT", headers, payload);
     }
 
     public String delete(String url, Map<String, String> headers, String payload) {
-        return method(url, "DELETE", headers, payload);
+        return execute(url, "DELETE", headers, payload);
     }
 
-    private String method(String url, String method, Map<String, String> headers, String payload) {
+    private String execute(String url, String method, Map<String, String> headers, String payload) {
         Request.Builder builder = new Request.Builder()
             .url(url);
 
@@ -68,7 +67,7 @@ public class HttpClient {
             headers.forEach(builder::addHeader);
         }
 
-        Request request = null;
+        Request request;
         if ("GET".equals(method)) {
             request = builder.method(method, null).build();
         } else {
@@ -76,9 +75,11 @@ public class HttpClient {
             request = builder.method(method, body).build();
         }
 
-        try {
-            Response response = okHttpClient.newCall(request).execute();
-            return response.body().string();
+        try (Response response = okHttpClient.newCall(request).execute()) {
+            ResponseBody body = response.body();
+            if (body != null) {
+                return body.string();
+            }
         } catch (Exception e) {
             LOG.error(e.toString(), e);
         }
