70c88dc7c34b2fd9f1a464728fde52db105a89e5
Michael Yang <fuhai999@gmail.com>
2024-05-30 19:20:44 +0800

    feat: optimize MilvusVectorStore samples

---
+++ agents-flex-samples/milvus-samples/src/main/java/com/agentsflex/store/milvus/MilvusStoreSample1.java
@@ -0,0 +1,36 @@
+package com.agentsflex.store.milvus;
+
+import com.agentsflex.document.Document;
+import com.agentsflex.llm.Llm;
+import com.agentsflex.llm.spark.SparkLlm;
+import com.agentsflex.llm.spark.SparkLlmConfig;
+import com.agentsflex.store.SearchWrapper;
+import com.agentsflex.store.StoreOptions;
+import com.agentsflex.store.StoreResult;
+
+import java.util.List;
+
+public class MilvusStoreSample1 {
+
+    public static void main(String[] args) {
+        SparkLlmConfig sparkLlmConfig = new SparkLlmConfig();
+        sparkLlmConfig.setAppId("****");
+        sparkLlmConfig.setApiKey("****");
+        sparkLlmConfig.setApiSecret("****");
+
+        Llm llm = new SparkLlm(sparkLlmConfig);
+
+        MilvusVectorStoreConfig config = new MilvusVectorStoreConfig();
+        config.setUri("http://127.0.0.1:19530");
+        MilvusVectorStore store = new MilvusVectorStore(config);
+        store.setEmbeddingModel(llm);
+
+        StoreResult result = store.store(Document.of("test"), StoreOptions.ofCollectionName("default01"));
+        System.out.println(result);
+
+        SearchWrapper wrapper = new SearchWrapper();
+        wrapper.text("test");
+        List<Document> search = store.search(wrapper,StoreOptions.ofCollectionName("default01"));
+        System.out.println(search);
+    }
+}
