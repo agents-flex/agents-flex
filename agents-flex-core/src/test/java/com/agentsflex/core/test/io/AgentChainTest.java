d92b5d12ef14335bc926218b57627f58c12f01e2
Michael Yang <fuhai999@gmail.com>
2024-07-07 16:43:32 +0800

    feat: add LoopChain test

--- agents-flex-core/src/test/java/com/agentsflex/core/test/io/AgentChainTest.java
+++ agents-flex-core/src/test/java/com/agentsflex/core/test/io/AgentChainTest.java
@@ -4,11 +4,14 @@ import com.agentsflex.core.chain.Chain;
 import com.agentsflex.core.chain.ChainEvent;
 import com.agentsflex.core.chain.ChainEventListener;
 import com.agentsflex.core.chain.ChainException;
+import com.agentsflex.core.chain.impl.LoopChain;
 import com.agentsflex.core.chain.impl.SequentialChain;
+import org.junit.Test;
 
 public class AgentChainTest {
 
-    public static void main(String[] args) throws ChainException {
+    @Test
+    public void testSequential() throws ChainException {
 
         SequentialChain chain1 = new SequentialChain();
         chain1.addNode(new Agent1("agent1"));
@@ -28,6 +31,21 @@ public class AgentChainTest {
             }
         });
 
+
+        Object result = chain1.executeForResult("your params");
+        System.out.println(">>>>>" + result);
+    }
+
+
+    @Test
+    public void testLoop() throws ChainException {
+
+        LoopChain chain1 = new LoopChain();
+        chain1.addNode(new Agent1("agent1"));
+        chain1.addNode(new Agent2("agent2"));
+
+        chain1.close();
+        chain1.setMaxLoopCount(10);
 
         Object result = chain1.executeForResult("your params");
         System.out.println(">>>>>" + result);
