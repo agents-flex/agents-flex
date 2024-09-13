8fc1d9a8bf3277c48dab216e54e075a87a72d739
Michael Yang <fuhai999@gmail.com>
2024-09-13 14:13:49 +0800

    test: add MapsTest.java

---
+++ agents-flex-core/src/test/java/com/agentsflex/core/test/util/MapsTest.java
@@ -0,0 +1,20 @@
+package com.agentsflex.core.test.util;
+
+import com.agentsflex.core.util.Maps;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class MapsTest {
+
+    @Test
+    public void testMaps() {
+        Map<String, Object> map1 = Maps.of("key", "value")
+            .put("options.aaa", 1)
+            .build();
+
+        Assert.assertEquals(1, ((Map<?, ?>) map1.get("options")).get("aaa"));
+        System.out.println(map1);
+    }
+}
