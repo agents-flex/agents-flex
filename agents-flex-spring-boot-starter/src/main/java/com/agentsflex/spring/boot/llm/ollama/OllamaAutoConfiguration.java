bff56bab93a1ac130394c189e267d935af44d4e7
greenfivebird <53175188+greenfivebird@users.noreply.github.com>
2025-02-11 11:56:54 +0800

    增加Springboot中Ollama自动配置类

---
+++ agents-flex-spring-boot-starter/src/main/java/com/agentsflex/spring/boot/llm/ollama/OllamaAutoConfiguration.java
@@ -0,0 +1,34 @@
+package com.agentsflex.spring.boot.llm.ollama;
+
+import com.agentsflex.llm.moonshot.MoonshotLlm;
+import com.agentsflex.llm.moonshot.MoonshotLlmConfig;
+import com.agentsflex.llm.ollama.OllamaLlm;
+import com.agentsflex.llm.ollama.OllamaLlmConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Agents-Flex Ollama自动配置。
+ *
+ * @author hustlelr
+ * @since 2025-02-11
+ */
+@ConditionalOnClass(OllamaLlm.class)
+@Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(OllamaProperties.class)
+public class OllamaAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public OllamaLlm ollamaLlm(OllamaProperties properties) {
+        OllamaLlmConfig config = new OllamaLlmConfig();
+        config.setApiKey(properties.getApiKey());
+        config.setEndpoint(properties.getEndpoint());
+        config.setModel(properties.getModel());
+        return new OllamaLlm(config);
+    }
+
+}
