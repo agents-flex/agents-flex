5470e6d63c0c847e87dce0554e2a943f2339b0bc
sunchanghui <843961586@qq.com>
2025-03-04 13:49:06 +0800

    feat:接入腾讯大模型语言与向量化接口

---
+++ agents-flex-llm/agents-flex-llm-tencent/src/main/java/com/agentsflex/llm/chatglm/TencentlmLlm.java
@@ -0,0 +1,106 @@
+/*
+ *  Copyright (c) 2023-2025, Agents-Flex (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.agentsflex.llm.chatglm;
+
+import com.agentsflex.core.document.Document;
+import com.agentsflex.core.llm.BaseLlm;
+import com.agentsflex.core.llm.ChatOptions;
+import com.agentsflex.core.llm.StreamResponseListener;
+import com.agentsflex.core.llm.client.BaseLlmClientListener;
+import com.agentsflex.core.llm.client.HttpClient;
+import com.agentsflex.core.llm.client.LlmClient;
+import com.agentsflex.core.llm.client.LlmClientListener;
+import com.agentsflex.core.llm.client.impl.DnjsonClient;
+import com.agentsflex.core.llm.embedding.EmbeddingOptions;
+import com.agentsflex.core.llm.response.AiMessageResponse;
+import com.agentsflex.core.parser.AiMessageParser;
+import com.agentsflex.core.prompt.Prompt;
+import com.agentsflex.core.store.VectorData;
+import com.agentsflex.core.util.Maps;
+import com.agentsflex.core.util.StringUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPath;
+
+import java.util.Map;
+
+public class TencentlmLlm extends BaseLlm<TencentLlmConfig> {
+
+    private HttpClient httpClient = new HttpClient();
+    public AiMessageParser aiMessageParser = TencentLlmUtil.getAiMessageParser(false);
+    public AiMessageParser aiStreamMessageParser = TencentLlmUtil.getAiMessageParser(true);
+
+    public TencentlmLlm(TencentLlmConfig config) {
+        super(config);
+    }
+
+    /**
+     * 文档参考：https://cloud.tencent.com/document/product/1729/101841
+     *
+     * @param document 文档内容
+     * @return 返回向量数据
+     */
+    @Override
+    public VectorData embed(Document document, EmbeddingOptions options) {
+        String payload = Maps.of("Input", document.getContent()).toJSON();
+        Map<String, String> headers = TencentLlmUtil.createAuthorizationToken(config, "GetEmbedding", payload);
+        String response = httpClient.post("https://" + config.getEndpoint(), headers, payload);
+        if (config.isDebug()) {
+            System.out.println(">>>>receive payload:" + response);
+        }
+        if (StringUtil.noText(response)) {
+            return null;
+        }
+        VectorData vectorData = new VectorData();
+        vectorData.setVector(JSONPath.read(response, "$.Response.Data[0].Embedding", double[].class));
+        return vectorData;
+    }
+
+    @Override
+    public AiMessageResponse chat(Prompt prompt, ChatOptions options) {
+        String payload = TencentLlmUtil.promptToPayload(prompt, config, false, options);
+        Map<String, String> headers = TencentLlmUtil.createAuthorizationToken(config, "ChatCompletions", payload);
+        String response = httpClient.post("https://" + config.getEndpoint(), headers, payload);
+        if (config.isDebug()) {
+            System.out.println(">>>>receive payload:" + response);
+        }
+        if (StringUtil.noText(response)) {
+            return AiMessageResponse.error(prompt, response, "no content for response.");
+        }
+
+        JSONObject jsonObject = JSON.parseObject(response);
+        JSONObject error = jsonObject.getJSONObject("Response").getJSONObject("Error");
+        AiMessageResponse messageResponse = new AiMessageResponse(prompt, response, aiMessageParser.parse(jsonObject));
+        if (error != null && !error.isEmpty()) {
+            messageResponse.setError(true);
+            messageResponse.setErrorMessage(error.getString("Message"));
+            messageResponse.setErrorType(error.getString("type"));
+            messageResponse.setErrorCode(error.getString("Code"));
+        }
+        return messageResponse;
+    }
+
+
+    @Override
+    public void chatStream(Prompt prompt, StreamResponseListener listener, ChatOptions options) {
+        LlmClient llmClient = new DnjsonClient();
+        String payload = TencentLlmUtil.promptToPayload(prompt, config, true, options);
+        Map<String, String> headers = TencentLlmUtil.createAuthorizationToken(config,"ChatCompletions", payload);
+        LlmClientListener clientListener = new BaseLlmClientListener(this, llmClient, listener, prompt, aiStreamMessageParser);
+        llmClient.start("https://" + config.getEndpoint(), headers, payload, clientListener, config);
+    }
+
+}
---
+++ agents-flex-llm/agents-flex-llm-tencent/src/test/java/com/agentsflex/llm/chatglm/test/ChatGlmTest.java
@@ -0,0 +1,78 @@
+package com.agentsflex.llm.chatglm.test;
+
+import com.agentsflex.core.document.Document;
+import com.agentsflex.core.llm.ChatContext;
+import com.agentsflex.core.llm.Llm;
+import com.agentsflex.core.llm.StreamResponseListener;
+import com.agentsflex.core.llm.embedding.EmbeddingOptions;
+import com.agentsflex.core.llm.response.AiMessageResponse;
+import com.agentsflex.core.prompt.FunctionPrompt;
+import com.agentsflex.core.store.VectorData;
+import com.agentsflex.llm.chatglm.TencentLlmConfig;
+import com.agentsflex.llm.chatglm.TencentlmLlm;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class ChatGlmTest {
+
+
+    public static void main(String[] args) throws Exception {
+//        TencentLlmConfig config = new TencentLlmConfig();
+//        config.setApiSecret("********");
+//        config.setApiKey("******");
+//        Llm llm = new TencentlmLlm(config);
+//        String response = llm.chat("你好");
+//        System.out.println(response);
+
+        TencentLlmConfig config = new TencentLlmConfig();
+        config.setApiSecret("********");
+        config.setApiKey("******");
+        Llm llm = new TencentlmLlm(config);
+        llm.chatStream("what is your name?", new StreamResponseListener() {
+            @Override
+            public void onMessage(ChatContext context, AiMessageResponse response) {
+                System.out.println("onMessage=====" + response.getMessage().getContent());
+            }
+
+            @Override
+            public void onStop(ChatContext context) {
+                System.out.println("停止");
+            }
+
+            @Override
+            public void onFailure(ChatContext context, Throwable throwable) {
+                System.out.println("出错" + throwable.getMessage());
+            }
+        });
+    }
+
+
+    @Test
+    public void testEmbedding() {
+        TencentLlmConfig config = new TencentLlmConfig();
+        config.setApiSecret("AKIDLYZN1iJjwBLNCoERXQ6YqvofR2Rj1ABI");
+        config.setApiKey("EJH4U4lcHjVBlEWebOzBMM9lBOOP1Tkq");
+        Llm llm = new TencentlmLlm(config);
+        Document document = new Document();
+        document.setContent("你好");
+        VectorData embeddings = llm.embed(document, EmbeddingOptions.DEFAULT);
+        System.out.println(Arrays.toString(embeddings.getVector()));
+    }
+
+
+    @Test
+    public void testFunctionCalling() {
+        TencentLlmConfig config = new TencentLlmConfig();
+        config.setApiSecret("******");
+        config.setApiKey("***********");
+
+        Llm llm = new TencentlmLlm(config);
+
+        FunctionPrompt prompt = new FunctionPrompt("今天北京的天气怎么样", WeatherFunctions.class);
+        AiMessageResponse response = llm.chat(prompt);
+
+        System.out.println(response.callFunctions());
+    }
+
+}
