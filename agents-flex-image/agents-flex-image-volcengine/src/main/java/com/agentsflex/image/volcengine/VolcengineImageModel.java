860b26c5e2083d0529e4467ba44efc849a0c430a
DESKTOP-R1P7VVC\85856 <858567089@qq.com>
2025-03-06 14:49:15 +0800

    优化代码结构

--- agents-flex-image/agents-flex-image-volcengine/src/main/java/com/agentsflex/image/volcengine/VolcengineImageModel.java
+++ agents-flex-image/agents-flex-image-volcengine/src/main/java/com/agentsflex/image/volcengine/VolcengineImageModel.java
@@ -2,88 +2,56 @@ package com.agentsflex.image.volcengine;
 
 
 import com.agentsflex.core.image.*;
-import com.agentsflex.core.llm.client.HttpClient;
-import com.agentsflex.core.util.Maps;
-import com.agentsflex.core.util.StringUtil;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson2.JSON;
 import com.volcengine.service.visual.IVisualService;
 import com.volcengine.service.visual.impl.VisualServiceImpl;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+
 
 public class VolcengineImageModel implements ImageModel {
 
     private VolcengineImageModelConfig config;
-    private HttpClient httpClient = new HttpClient();
+    private IVisualService visualService = VisualServiceImpl.getInstance();
 
     public VolcengineImageModel(VolcengineImageModelConfig config) {
         this.config = config;
+        visualService.setAccessKey(config.getAccessKey());
+        visualService.setSecretKey(config.getSecretKey());
+    }
+
+    private ImageResponse processImageRequest(GenerateImageRequest request) {
+        JSONObject req = new JSONObject(request.getOptions());
+        ImageResponse responseimage = new ImageResponse();
+        try {
+            Object response = visualService.cvProcess(req);
+            if (response instanceof JSONObject) {
+                JSONObject jsonResponse = (JSONObject) response;
+                JSONObject dataObject = jsonResponse.getJSONObject("data");
+                JSONArray imageUrlsArray = dataObject.getJSONArray("image_urls");
+                for (int i = 0; i < imageUrlsArray.size(); i++) {
+                    responseimage.addImage(imageUrlsArray.getString(i));
+                }
+            } else {
+                throw new RuntimeException("Unexpected response type: " + response.getClass().getName());
+            }
+            return responseimage;
+        } catch (Exception e) {
+            ImageResponse.error(e.getMessage());
+            e.printStackTrace(); // 记录堆栈跟踪方便调试
+            return responseimage;
+        }
     }
 
 
     @Override
     public ImageResponse generate(GenerateImageRequest request) {
-        IVisualService visualService = VisualServiceImpl.getInstance();
-
-        visualService.setAccessKey(config.getAccessKey());
-        visualService.setSecretKey(config.getSecretKey());
-
-        JSONObject req = new JSONObject(request.getOptions());
-        ImageResponse responseimage = new ImageResponse();
-        Object response = null;
-        try {
-            response = visualService.cvProcess(req);
-
-            if (response instanceof JSONObject) {
-                JSONObject jsonResponse = (JSONObject) response;
-                // 获取"data"对象
-                JSONObject dataObject = jsonResponse.getJSONObject("data");
-                // 获取"image_urls"数组
-                JSONArray imageUrlsArray = dataObject.getJSONArray("image_urls");
-                // 遍历并打印"image_urls"数组中的每个URL
-                for (int i = 0; i < imageUrlsArray.size(); i++) {
-                    responseimage.addImage(imageUrlsArray.getString(i));
-                }
-
-            }
-            return responseimage;
-        } catch(Exception e) {
-           return responseimage.error(e.getMessage());
-        }
+        return processImageRequest(request);
     }
 
     @Override
     public ImageResponse img2imggenerate(GenerateImageRequest request) {
-        IVisualService visualService = VisualServiceImpl.getInstance();
-        // call below method if you dont set ak and sk in ～/.vcloud/config
-        visualService.setAccessKey(config.getAccessKey());
-        visualService.setSecretKey(config.getSecretKey());
-
-        JSONObject req = new JSONObject(request.getOptions());
-        ImageResponse responseimage = new ImageResponse();
-        Object response = null;
-        try {
-            response = visualService.cvProcess(req);
-            if (response instanceof JSONObject) {
-                JSONObject jsonResponse = (JSONObject) response;
-                // 获取"data"对象
-                JSONObject dataObject = jsonResponse.getJSONObject("data");
-                // 获取"image_urls"数组
-                JSONArray imageUrlsArray = dataObject.getJSONArray("image_urls");
-                // 遍历并打印"image_urls"数组中的每个URL
-                for (int i = 0; i < imageUrlsArray.size(); i++) {
-                    responseimage.addImage(imageUrlsArray.getString(i));
-                }
-
-            }
-            return responseimage;
-        } catch(Exception e) {
-            return responseimage.error(e.getMessage());
-        }
+        return processImageRequest(request);
     }
 
 
