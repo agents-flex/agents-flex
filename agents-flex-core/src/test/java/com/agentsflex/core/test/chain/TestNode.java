d70c52f9d6bc945287be3a9403f9fc934f826e38
Michael Yang <fuhai999@gmail.com>
2025-04-25 12:10:14 +0800

    test: add ChainHolderTest

--- agents-flex-core/src/test/java/com/agentsflex/core/test/chain/TestNode.java
+++ agents-flex-core/src/test/java/com/agentsflex/core/test/chain/TestNode.java
@@ -3,14 +3,15 @@ package com.agentsflex.core.test.chain;
 import com.agentsflex.core.chain.Chain;
 import com.agentsflex.core.chain.node.BaseNode;
 
-import java.util.Collections;
 import java.util.Map;
 
 public class TestNode extends BaseNode {
 
     @Override
     protected Map<String, Object> execute(Chain chain) {
+        System.out.println("TestNode.execute: " + this.id);
         Map<String, Object> parameterValues = chain.getParameterValues(this);
-        return Collections.emptyMap();
+        System.out.println("TestNode.values: " + parameterValues);
+        return parameterValues;
     }
 }
