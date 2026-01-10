8236148b9cc376ea814bc09a3ea578a7d4370fac
Cennac <cennac@163.com>
2026-01-10 15:13:58 +0000

    add OllamaRequestSpecBuilder.java.
    Signed-off-by: Cennac <cennac@163.com>

---
+++ agents-flex-chat/agents-flex-chat-ollama/src/main/java/com/agentsflex/llm/ollama/OllamaRequestSpecBuilder.java
@@ -0,0 +1,23 @@
+package com.agentsflex.llm.ollama;
+
+import com.agentsflex.core.model.chat.ChatConfig;
+import com.agentsflex.core.model.chat.ChatOptions;
+import com.agentsflex.core.model.client.OpenAIChatRequestSpecBuilder;
+import com.agentsflex.core.prompt.Prompt;
+import com.agentsflex.core.util.CollectionUtil;
+import com.agentsflex.core.util.Maps;
+
+public class OllamaRequestSpecBuilder extends OpenAIChatRequestSpecBuilder {
+    protected Maps buildBaseParamsOfRequestBody(Prompt prompt, ChatOptions options, ChatConfig config) {
+        Maps params = super.buildBaseParamsOfRequestBody(prompt, options, config);
+        params.setIf(!options.isStreaming(), "stream", false);
+
+        // 支持思考
+        if(config.getSupportThinking()){
+            params.setIf(options.getThinkingEnabled() != null, "think", options.getThinkingEnabled());
+        }
+        return params;
+
+
+    }
+}
