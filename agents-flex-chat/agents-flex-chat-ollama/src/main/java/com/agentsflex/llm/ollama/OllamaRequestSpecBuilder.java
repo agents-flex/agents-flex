737207cb045bc4a74a13cb783f9bc33973c2cccc
Michael Yang <fuhai999@gmail.com>
2026-01-12 11:08:29 +0800

    chore(ollama): clean up request spec builder code

--- agents-flex-chat/agents-flex-chat-ollama/src/main/java/com/agentsflex/llm/ollama/OllamaRequestSpecBuilder.java
+++ agents-flex-chat/agents-flex-chat-ollama/src/main/java/com/agentsflex/llm/ollama/OllamaRequestSpecBuilder.java
@@ -4,7 +4,6 @@ import com.agentsflex.core.model.chat.ChatConfig;
 import com.agentsflex.core.model.chat.ChatOptions;
 import com.agentsflex.core.model.client.OpenAIChatRequestSpecBuilder;
 import com.agentsflex.core.prompt.Prompt;
-import com.agentsflex.core.util.CollectionUtil;
 import com.agentsflex.core.util.Maps;
 
 public class OllamaRequestSpecBuilder extends OpenAIChatRequestSpecBuilder {
@@ -13,11 +12,10 @@ public class OllamaRequestSpecBuilder extends OpenAIChatRequestSpecBuilder {
         params.setIf(!options.isStreaming(), "stream", false);
 
         // 支持思考
-        if(config.getSupportThinking()){
+        if (config.getSupportThinking()) {
             params.setIf(options.getThinkingEnabled() != null, "think", options.getThinkingEnabled());
         }
+
         return params;
-
-
     }
 }
