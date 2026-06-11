094aefce8a5584aaf3983762460583cd5410f0df
Michael Yang <fuhai999@gmail.com>
2026-06-11 10:14:43 +0800

    build: v2.1.8 release (^.^)YYa!!

--- agents-flex-image/agents-flex-image-qianfan/src/test/java/com/agentsflex/image/test/QianfanImageModelTest.java
+++ agents-flex-image/agents-flex-image-qianfan/src/test/java/com/agentsflex/image/test/QianfanImageModelTest.java
@@ -4,11 +4,9 @@ import com.agentsflex.core.model.image.GenerateImageRequest;
 import com.agentsflex.core.model.image.ImageResponse;
 import com.agentsflex.image.qianfan.QianfanImageModel;
 import com.agentsflex.image.qianfan.QianfanImageModelConfig;
-import org.junit.Test;
 
 public class QianfanImageModelTest {
 
-    @Test
     public void testGenerate() throws InterruptedException {
         QianfanImageModelConfig config = new QianfanImageModelConfig();
         config.setApiKey("*************");
