1a1d3aa9f3afc66e8c7faa86c55eea707ea68423
Michael Yang <fuhai999@gmail.com>
2026-06-24 17:39:42 +0800

    test(volc): add file output functionality to audio streaming tests

--- agents-flex-audio/agents-flex-audio-volc/src/test/java/com/agentsflex/audio/volc/VolcAudioTest.java
+++ agents-flex-audio/agents-flex-audio-volc/src/test/java/com/agentsflex/audio/volc/VolcAudioTest.java
@@ -7,6 +7,8 @@ import com.agentsflex.core.audio.tts.*;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class VolcAudioTest {
 
@@ -18,6 +20,8 @@ public class VolcAudioTest {
         TextToSpeechModel model = new VolcTextToSpeechModel(config);
         TextToSpeechResponse helloWorld = model.tts(new TextToSpeechRequest("hello world"));
 
+        helloWorld.writeTo(new File("/Users/michael/Desktop/helloWorld.mp3"));
+
         System.out.println(helloWorld);
     }
 
@@ -25,10 +29,12 @@ public class VolcAudioTest {
     public void testTextToSpeechStreaming() throws InterruptedException {
         VolcTextToSpeechConfig config = new VolcTextToSpeechConfig();
         config.setApiKey(System.getenv("VOLC_API_KEY"));
+        File tempFile = new File("/Users/michael/Desktop/aaa.mp3");
 
         try (StreamingTextToSpeechModel model = new VolcStreamingTextToSpeechModel(config)) {
             model.init(new TextToSpeechOptions());
             model.addListener(new StreamingTextToSpeechListener() {
+                FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
                 @Override
                 public void onStart() {
                     System.out.println("start");
@@ -37,6 +43,11 @@ public class VolcAudioTest {
                 @Override
                 public void onReceived(byte[] bytes) {
                     System.out.println("received: " + bytes);
+                    try {
+                        fileOutputStream.write( bytes);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
 
                 @Override
@@ -47,12 +58,18 @@ public class VolcAudioTest {
                 @Override
                 public void onComplete() {
                     System.out.println("complete");
+                    try {
+                        fileOutputStream.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             });
 
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("send text: " + i);
                 model.sendText("hello world");
-                Thread.sleep(1000L);
+                Thread.sleep(200L);
             }
 
 //            Thread.sleep(1000L * 10);
@@ -60,7 +77,8 @@ public class VolcAudioTest {
             throw new RuntimeException(e);
         }
 
-//        Thread.sleep(1000L * 10);
+        System.out.println(tempFile);
+
 
     }
 
